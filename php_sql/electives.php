c021e1348ebeaf7b3873a58122740e597ea7a042
Diana Georgieva <40742204+DianaGeorgievaa@users.noreply.github.com>
2020-04-29 03:38:01 +0700

    Fix comments from code review

--- php_sql/electives.php
+++ php_sql/electives.php
@@ -1,59 +1,34 @@
 <?php
+
 define("MAX_TITLE_LENGTH", 128, true);
 define("MAX_TEACHER_LENGTH", 1024, true);
-define("MIN_DESCRIPTION_LENGTH", 10, true);
+define("MAX_DESCRIPTION_LENGTH", 1024, true);
+
+$valid = array();
+$errors = array();
 
 $host = "localhost";
 $username = "root";
 $pass = "";
 $dbname = "test";
 
-function validateTitle($title, &$errors)
-{
-  if (!$title) {
-    $errors['title'] = "Името е задължително поле.";
-  } elseif (strlen($title) > MAX_TITLE_LENGTH) {
-    $errors['title'] = "Името има максимална дължина 150 символа.";
-  }
-  if (array_key_exists('title', $errors)) {
-    echo $errors['title'];
-  }
+function validateFormField(String $formField, int $maxLength, &$valid, &$errors){
+  $inputValue = $_POST["$formField"];
+  if(!$inputValue){
+      $errors["$formField"] = "Полето е задължително."; 
+  } elseif(strlen($inputValue) > $maxLength){
+      $errors["$formField"] = "Полето има максимална дължина символа.";
+  } else {
+      $valid[$formField] = $inputValue;
+  }        
 }
 
-function validateTeacher($teacher, &$errors)
-{
-  if (!$teacher) {
-    $errors['teacher'] = "Името на преподавател е задължително поле.";
-  } elseif (strlen($teacher) > MAX_TEACHER_LENGTH) {
-    $errors['teacher'] = "Името на преподавател има максимална дължина 200 символа.";
-  } 
-   if (array_key_exists('teacher', $errors)) {
-    echo $errors['teacher'];
-  }
-}
 
-function validateDescription($description, &$errors)
-{
-  if (!$description) {
-    $errors['description'] = "Описанието на дисциплината е задължително поле.";
-  } elseif (strlen($description) < MIN_DESCRIPTION_LENGTH) {
-    $errors['description'] = "Описанието на дисциплината има минимална дължина 10 символа.";
-  } 
-    if (array_key_exists('description', $errors)) {
-    echo $errors['description'];
-  }
-}
-
-if ($_POST) {
-  $errors = array();
-
-  $description = $_POST['description'];
-  $teacher = $_POST['teacher'];
-  $title = $_POST['title'];
-
-  validateTitle($title, $errors);
-  validateTeacher($teacher, $errors);
-  validateDescription($description, $errors);
+if( $_POST ){
+  
+  validateFormField('title', MAX_TITLE_LENGTH, $valid, $errors);
+  validateFormField('teacher',MAX_TEACHER_LENGTH, $valid, $errors);
+  validateFormField('description',MAX_DESCRIPTION_LENGTH, $valid, $errors);
 
   if (count($errors) === 0) {
     echo "The form was successfully submitted!";
